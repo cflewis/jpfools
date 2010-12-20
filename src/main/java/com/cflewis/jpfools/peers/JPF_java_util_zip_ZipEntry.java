d44858d31df3a06dfb0d6402aff58e32f561cffd
Chris Lewis <chris@chris.to>
2010-12-20 11:48:39 -0800

    Committing before cleanup

---
+++ src/main/java/com/cflewis/jpfools/peers/JPF_java_util_zip_ZipEntry.java
@@ -0,0 +1,9 @@
+package com.cflewis.jpfools.peers;
+
+import gov.nasa.jpf.jvm.MJIEnv;
+
+public class JPF_java_util_zip_ZipEntry {
+    public static void $clinit (MJIEnv env, int rcls) {
+        // Do nothing
+    }
+}
