be18adeba49cb573720c8450f68bad211eb66166
Chris Lewis <chris@chris.to>
2010-12-20 16:06:06 -0800

    Actually works

---
+++ src/main/java/com/cflewis/jpfools/Runner.java
@@ -0,0 +1,10 @@
+package com.cflewis.jpfools;
+
+public class Runner {
+    private static void greet() {
+        System.out.println("Hello world!");
+    }
+    public static void main(String args[]) {
+        greet();
+    }
+}
--- src/main/java/com/cflewis/jpfools/peers/JPF_java_util_zip_ZipEntry.java
+++
@@ -1,9 +0,0 @@
-package com.cflewis.jpfools.peers;
-
-import gov.nasa.jpf.jvm.MJIEnv;
-
-public class JPF_java_util_zip_ZipEntry {
-    public static void $clinit (MJIEnv env, int rcls) {
-        // Do nothing
-    }
-}
